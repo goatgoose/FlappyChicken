0afde4d82f2fbfd3afc31d61f6417b8a3820a447
Sam Clark <sam.clark1142@gmail.com>
2014-03-05 03:28:56 -0500

    template

---
+++ src/com/goatgoose/flappychicken/FlappyChicken.java
@@ -0,0 +1,17 @@
+package com.goatgoose.flappychicken;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FlappyChicken extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+
+}
