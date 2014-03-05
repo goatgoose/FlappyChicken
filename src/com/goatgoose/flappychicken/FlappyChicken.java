e7a91c96e55f9256b5ef828223ae0c404c073b89
Sam Clark <sam.clark1142@gmail.com>
2014-03-05 04:18:38 -0500

    initial physics test

--- src/com/goatgoose/flappychicken/FlappyChicken.java
+++ src/com/goatgoose/flappychicken/FlappyChicken.java
@@ -1,6 +1,14 @@
 package com.goatgoose.flappychicken;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Chicken;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.util.Vector;
 
 public class FlappyChicken extends JavaPlugin {
 
@@ -11,7 +19,25 @@ public class FlappyChicken extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        Player player = (Player) sender;
+        // temp testing
+        if(command.getName().equalsIgnoreCase("test")) {
+            final Chicken testChicken = (Chicken) player.getWorld().spawnEntity(player.getLocation(), EntityType.CHICKEN);
+            BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+            scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+                @Override
+                public void run() {
+                    testChicken.setVelocity(new Vector(0, 0.2, -0.3));
+                }
+            }, 0, 1);
+            return true;
+        }
+        return false;
     }
 
 }
